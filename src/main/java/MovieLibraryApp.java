cd3e9d9d74b66903f67e5095e87e0ef66940b23f
Paweł Żychski <pawel.zychski@gmail.com>
2022-05-15 12:15:58 +0200

    Add model class: Actor, Director, Movie

--- src/main/java/MovieLibraryApp.java
+++ src/main/java/MovieLibraryApp.java
@@ -1,2 +1,11 @@
+import models.Actor;
+
 public class MovieLibraryApp {
+
+    public static void main(String[] args) {
+        Actor salma = new Actor("Salma", "Hayek");
+
+        System.out.println(salma);
+    }
 }
+
